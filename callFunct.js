9ebbf38d61a20b4613ab3934d82f2032e9fddba3
Milanocka- <matildarumc@gmail.com>
2024-10-02 13:09:55 +0300

    mend

--- callFunct.js
+++ callFunct.js
@@ -7,23 +7,3 @@ function myFn (a, b) {
 const e = myFn(3, 5);
 
 console.log(e);
-
-function myFn (a, b) {
-    let c = a + b
-    a = a + 1
-    b = b + 1
-    return c
-}
-const e = myFn(3, 5);
-
-console.log(e);
-
-function myFn (a, b) {
-    let c = a + b
-    a = a + 1
-    b = b + 1
-    return c
-}
-const e = myFn(3, 5);
-
-console.log(e);
